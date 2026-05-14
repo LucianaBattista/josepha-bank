ffb417bec85459551b541a8f0e3694942a84772d
Luciana Batista <lucianabattista@gmail.com>
2026-05-14 14:40:37 -0300

    corrige frontend para render

--- backend/server.py
+++ backend/server.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=
     ["http://localhost:3000"
-     "https://josepha-bank.onrender.com"],
+     "https://josepha-bank.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
